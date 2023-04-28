ced1823277ddf3e32b84c47e483d62ccee7413c9
zhaoyukun <zhaoyukun@2woniu.com>
2023-04-28 17:14:12 +0800

    完成tabbar的功能开发

---
+++ pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    这是home页
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
